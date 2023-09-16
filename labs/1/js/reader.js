7232b45cbe7b58f76a0e6110247f626138c5026e
Sepehr Zohoori Rad <zohooris1@gmail.com>
2023-09-15 22:32:11 -0700

    add comments

--- labs/1/js/reader.js
+++ labs/1/js/reader.js
@@ -1,28 +1,38 @@
+// Add an event listener to ensure the script runs after the DOM is fully loaded
 document.addEventListener('DOMContentLoaded', function() {
+    
+    // Grab the main container where texts will be displayed and the element for last read timestamp
     const container = document.getElementById('textsContainer');
     const lastReadEl = document.getElementById('lastRead');
 
-    // Go back to index.html
+    // Set up functionality to navigate back to the main index.html
     const backBtn = document.getElementById('backButton');
     backBtn.addEventListener('click', function() {
         window.location.href = "index.html";
     });
 
+    // Function to fetch stored texts from local storage and display them
     function updateFromLocalStorage() {
-        container.innerHTML = ''; // Clear previous content
+        container.innerHTML = ''; // Clear the container of any previous content
+
+        // Retrieve texts from local storage or use an empty array if no texts are found
         const storedTexts = JSON.parse(localStorage.getItem('textAreasContent') || '[]');
 
+        // For each stored text, create a new div and set its content, then append it to the container
         storedTexts.forEach(text => {
             const div = document.createElement('div');
             div.textContent = text;
             container.appendChild(div);
         });
 
-        // Update the "Last read" time
+        // Update the element indicating the last time the data was read from local storage
         const now = new Date();
         lastReadEl.textContent = `Last read: ${now.toLocaleDateString()} ${now.toLocaleTimeString()}`;
     }
 
+    // Set an interval to update the displayed texts from local storage every 2 seconds
     setInterval(updateFromLocalStorage, 2000);
-    updateFromLocalStorage(); // Initial load
+
+    // Trigger an initial load of the texts
+    updateFromLocalStorage();
 });
--- labs/1/js/writer.js
+++ labs/1/js/writer.js
@@ -1,26 +1,35 @@
+// Add an event listener to ensure the script runs after the DOM is fully loaded
 document.addEventListener('DOMContentLoaded', function () {
+
+    // Grab necessary DOM elements
     const addBtn = document.getElementById('addButton');
     const container = document.getElementById('textAreasContainer');
     const lastSavedEl = document.getElementById('lastSaved');
     const backBtn = document.getElementById('backButton');
 
+    // Note constructor to create a textarea and associated remove button
     function Note(content = '') {
+        // Create a new textarea element and set its content
         this.textarea = document.createElement('textarea');
         this.textarea.value = content;
-        
-        this.removeBtn = document.createElement('button');
 
+        // Create a new remove button for the note
+        this.removeBtn = document.createElement('button');
         this.removeBtn.textContent = "Remove";
+
+        // Add an event listener to the remove button to delete the note and save the changes to local storage
         this.removeBtn.addEventListener('click', () => {
             container.removeChild(this.textarea);
             container.removeChild(this.removeBtn);
             saveToLocalStorage();
         });
 
+        // Append the textarea and remove button to the container
         container.appendChild(this.textarea);
         container.appendChild(this.removeBtn);
     }
 
+    // Load notes from local storage and render them
     function loadFromLocalStorage() {
         const savedTexts = JSON.parse(localStorage.getItem('textAreasContent')) || [];
         savedTexts.forEach(text => {
@@ -28,27 +37,36 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     }
 
+    // Event listener to navigate back to index.html when the back button is clicked
     backBtn.addEventListener('click', function () {
         window.location.href = "index.html";
     });
 
+    // Event listener to add a new note when the add button is clicked
     addBtn.addEventListener('click', function () {
         const note = new Note();
     });
 
+    // Save the current state of all textareas to local storage
     function saveToLocalStorage() {
         const texts = [];
+        
+        // Gather all textarea values into an array
         container.querySelectorAll('textarea').forEach(ta => {
             texts.push(ta.value);
         });
+
+        // Save the array of text values to local storage
         localStorage.setItem('textAreasContent', JSON.stringify(texts));
 
+        // Update the last saved timestamp
         const now = new Date();
         lastSavedEl.textContent = `Last saved: ${now.toLocaleDateString()} ${now.toLocaleTimeString()}`;
     }
 
+    // Save to local storage every 2 seconds
     setInterval(saveToLocalStorage, 2000);
 
-    // Load notes from local storage when page loads
+    // Load notes from local storage when the page initially loads
     loadFromLocalStorage();
 });
